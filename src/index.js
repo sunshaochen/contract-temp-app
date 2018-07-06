8daaa2347d2dd0286e3510382b6cfba594c77674
sunshaochen-cmbchina <sunscsmail@gmail.com>
2018-07-06 19:21:07 +0800

    修改项目结构

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './page/app';
+import App from './js/app';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
